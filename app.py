c726c49b4e6ca4db229334ac65915643f65354c9
Simon Redman <simon@ergotech.com>
2018-09-08 11:17:41 -0600

    Start trying to spoof a device pairing request.
    Current status is that the app crashes after clicking on the device to pair in the list of watches. This may be due to having skipped the EULA accept stage

--- app.py
+++ app.py
@@ -23,6 +23,77 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+@app.route('/api/v1/settings/PairedDeviceProfile', methods=['PUT'])
+def put_device_profile():
+    """
+    Called immediately after put_watch_MAC
+
+    For real, don't ask me what this is supposed to do, though
+    """
+    return
+
+
+@app.route('/api/v1/units/<string:watch_MAC>', methods=['PUT'])
+def put_watch_MAC(watch_MAC: str):
+    """
+    This is called immediately after clicking the watch in the "Connect by selecting your watch below" dialogue
+
+    It receives the watch's Bluetooth MAC address. What did Olio want with that?
+
+    Payload contains the user_id again
+
+    This returns a "com.olio.state.Unit" object. Goodness knows what those fields are supposed to contain...
+    """
+    user_id = request.json["user_id"]
+    firmware = {"bluetoothApkHash": None
+                , "bluetoothApkUrl": None
+                , "bluetoothApkVersion": "bt_apk_version"
+                , "defaultSettingsCollectionId": 4321
+                , "firmwareHash": None
+                , "firmwareUrl": None
+                , "id": user_id
+                , "name": "firmware_name"
+                , "uiApkHash": None
+                , "uiApkUrl": None
+                , "uiApkVersion": "ui_apk_version"
+                , "version": "firmware_version"}
+
+    product = {"band_style": "Steel"
+               , "body_style": "Steel"
+               , "brand": None
+               , "default_firmware": None
+               , "id": user_id
+               , "name": "product_name"
+               , "sku": "product_sku"}
+    current_look = {"asset_bundle_url": None
+                    , "current": True
+                    , "id": user_id
+                    , "name": "Look_name"
+                    , "product": product
+                    , "sample_watch_face": None}
+    user = {"email": "email@example.com"
+            , "firstName": "FirstName"
+            , "lastName": "LastName"
+            , "id": user_id}
+
+    response = {"current_look": current_look
+                , "firmware": firmware  # Could we fake a firmware update by punching this?
+                , "id": user_id
+                , "product": product
+                , "user": user}
+    return jsonify(response)
+
+
+@app.route('/api/v1/users/<int:user_id>/settings')
+def get_settings(user_id: int):
+    """
+    Handle a settings request
+    :param user_id: Whatever we sent the device as the reply to its get_me request
+    """
+    response = {}
+    return jsonify(response)
+
+
 @app.route('/api/v1/me', methods=['GET'])
 def get_me():
     """
