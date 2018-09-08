2932687b545660346799f4039d6cbfc1308d29fc
Simon Redman <simon@ergotech.com>
2018-09-08 11:15:19 -0600

    Implement enough REST API to spoof a login request

--- app.py
+++ app.py
@@ -18,15 +18,36 @@
 # You should have received a copy of the GNU General Public License
 # along with Foobar. If not, see <http://www.gnu.org/licenses/>.
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/api/v1/me', methods=['GET'])
+def get_me():
+    """
+    Handle a "me" request (Some naming convention, eh?)
+
+    I literally have no idea what this does except we get the ID back later
+    """
+    response = {"email": "haha@example.com"
+                , "id" : "1234"}
+    return jsonify(response)
+
+
+@app.route('/oauth/token', methods=['POST'])
+def login():
+    """
+    Handle a login request by returning an "auth token"
+    """
+    response = {"accessToken": "blarghaha"
+                ,"errorDescription": "Everything is good"}
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run()
+    key = 'pki/privkey.key'
+    cert = 'pki/CA.crt'
+    app.run(host='0.0.0.0'
+            , ssl_context=(cert, key)
+            , debug=True)
